082cf561f6dddda3ce611e5b328b1802b95da289
GLScene <pavelvvassiliev@gmail.com>
2023-03-21 00:04:09 +0300

    Upgraded structs and classes

---
+++ source/CGx.Register.pas
@@ -0,0 +1,39 @@
+//
+// The graphics engine GXScene https://github.com/glscene
+//
+unit CGx.Register;
+
+(*  Registration unit for CGx shader package *)
+
+interface
+
+{$I GLX.Scene.inc}
+
+uses
+  System.Classes,
+//  DesignIntf,
+//  DesignEditors,
+//  FMXEditors,
+
+  GLX.Material,
+
+  CGx.Import,
+  CGx.GL,
+  GLX.SceneRegister,  // TgxLibMaterialNameProperty
+  CGx.Shader,
+  CGx.BombShader;
+
+procedure Register;
+
+//-----------------------------------------------------
+implementation
+//-----------------------------------------------------
+
+procedure Register;
+begin
+  RegisterComponents('GXScene Shaders', [TCGxShader, TCGxBombShader]);
+  RegisterPropertyEditor(TypeInfo(TgxLibMaterialName), TCGxBombShader, '',
+             TgxLibMaterialNameProperty);
+end;
+
+end.
--- source/FMOD.Import.pas
+++ source/FMOD.Import.pas
@@ -25,7 +25,7 @@ unit FMOD.Import;
 
 interface
 
-{$I Scena.inc}
+{$I GLX.Scene.inc}
 
 uses
 
